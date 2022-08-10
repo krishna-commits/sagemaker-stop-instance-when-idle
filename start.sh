60743625308b6aa8a234e894e99b1ac432e9470e
Krishna <58653013+krishna-commits@users.noreply.github.com>
2022-08-10 10:53:17 +0545

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -ex
+
+# OVERVIEW
+# This script stops a SageMaker notebook once it's idle for more than 1 hour (default time)
+# You can change the idle time for stop using the environment variable below.
+# If you want the notebook the stop only if no browsers are open, remove the --ignore-connections flag
+#
+# Note that this script will fail if either condition is not met
+#   1. Ensure the Notebook Instance has internet connectivity to fetch the example config
+#   2. Ensure the Notebook Instance execution role permissions to SageMaker:StopNotebookInstance to stop the notebook 
+#       and SageMaker:DescribeNotebookInstance to describe the notebook.
+#
+
+# PARAMETERS
+IDLE_TIME=120
+
+echo "Fetching the autostop script"
+wget https://raw.githubusercontent.com/krishna-commits/sagemaker-stop-instance-when-idle/main/autostop.py
+
+
+echo "Detecting Python install with boto3 install"
+
+# Find which install has boto3 and use that to run the cron command. So will use default when available
+# Redirect stderr as it is unneeded
+if /usr/bin/python -c "import boto3" 2>/dev/null; then
+    PYTHON_DIR='/usr/bin/python'
+elif /usr/bin/python3 -c "import boto3" 2>/dev/null; then
+    PYTHON_DIR='/usr/bin/python3'
+else
+    # If no boto3 just quit because the script won't work
+    echo "No boto3 found in Python or Python3. Exiting..."
+    exit 1
+fi
+
+echo "Found boto3 at $PYTHON_DIR"
+
+
+echo "Starting the SageMaker autostop script in cron"
+
+(crontab -l 2>/dev/null; echo "*/5 * * * * $PYTHON_DIR $PWD/autostop.py --time $IDLE_TIME --ignore-connections >> /var/log/jupyter.log") | crontab
